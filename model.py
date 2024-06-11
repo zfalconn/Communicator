e82576aa6974d8a26b97d000785b43350444afd0
Linh Nguyen <166373732+zfalconn@users.noreply.github.com>
2024-06-11 15:41:11 +0200

    Simplified model.py

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 from typing import Any, Awaitable
 from asyncua import Client, Node
 import asyncio
+import time
 
 """
 To run async function, use await in front of this function.
@@ -13,34 +14,18 @@ class Connector:
 
     """
 
-    def __init__(self, opcua_url : str, node_id = None):
+    def __init__(self, opcua_url : str, node_ids : list): #maybe use strictly list for node_id in order to easily check input validity?
         """
         Create Connector object with specified url and node_id(s).
 
         Parameters:
             opcua_url(str) : OPCUA server url
-            node_ids(list or str) : List of node_ids to be monitored
+            node_ids(list of str) : List of node_ids to be monitored
         """
         
         self.url = opcua_url
         self.client = Client(opcua_url)
-        self.node_id = node_id
-        self.var = self.register_node()
-
-    def register_node(self):
-        """
-        Create Node object(s) from defined node_id(s). 
-        """
-
-        match self.node_id:
-            case list():
-                return [self.client.get_node(self.node_id[i]) for i in range(len(self.node_id))] #List of Nodes
-            case str():
-                return self.client.get_node(self.node_id)   #Single Node
-            case _:          
-                print("No node(s) assigned.")
-                return None
-   
+        self.var = self.register_node(node_ids)
 
     async def connect(self) -> bool: 
         
@@ -56,7 +41,6 @@ class Connector:
             print(f"An error has occurred: {e}")
             return False
 
-
     async def disconnect(self) -> bool:
         """
         Disconnect Client from OPCUA Server.
@@ -70,6 +54,15 @@ class Connector:
             print(f"An error has occurred: {e}")
             return False
     
+    def register_node(self, node_ids : list):
+        """
+        Create Node object(s) from defined node_id(s). 
+        """
+        try:
+            return [self.client.get_node(node_id) for node_id in node_ids]
+        except Exception as e:
+            print(f'An error occurred: {e}')   
+            
     def select_node(self, index : int = 0) -> Node:
         """
         Select specific Node using list indexing. Return same Node if only one Node is defined.
@@ -80,20 +73,12 @@ class Connector:
         Return:
             self.var(Node) : Node object
         """
-
-        match self.var:
-            case list():
-                #print(f"Selecting node {self.var[index]}.")
-                return self.var[index]
-            case _:
-                #print(f"Selecting node {self.var}.")
-                return self.var 
+        return self.var[index]
 
     async def read_value(self, index : int = 0):
         """
         Return value of at node_id.
         """
-        print(await self.select_node(index).read_value())
         return await self.select_node(index).read_value()
 
     def pubsub(self):
@@ -104,7 +89,7 @@ class Model:
     Model object use Connector object to access Nodes. Different Model can have different Connector.
     """
     
-    def __init__(self, model_id : str, connector : Connector = None):
+    def __init__(self, model_id : str, connector : Connector):
         """
         Create Model object.
 
@@ -115,55 +100,28 @@ class Model:
         
         self.model_id = model_id
         self.connector = connector
-        if self.connector is None:
-            print("Please connect to Client to Server.")
     
     def select_node(self, index : int = 0) -> Node:
         """
         Wrap select_node function of Connector class.
         """
-
         return self.connector.select_node(index)
 
-    async def send(self, message, index = None) -> None:
+    async def send(self, message, index : int = 0) -> None:
         """
         Change value of node_ID with the value of 'message'.
         Multiple messages can be sent to multiple nodes.
         """
-        match message:
-            case list():
-                if len(message) == len(index):
-                    await run_parallel(
-                        *[self.connector.select_node(i).write_value(message[i]) for i in index]
-                    )
-                    # for i in range(len(message)):
-                    #     await self.select_node(index[i]).write_value(message[i])
-                else:
-                    print("Length of message and node list must be the same.")
-            case _: 
-                await self.select_node(index).write_value(message)
-                # await asyncio.sleep(1)
+        await self.select_node(index).write_value(message)
 
-    async def send_coord(self, coord : tuple, index) -> None:
+    async def send_multiple(self, messages : list, indices : list) -> None:
         """
         Send coordinate (x,y) to two nodes
         """
-
-        if len(coord) != 2:
-            print("Coordinate must be a tuple of two values.")
-            return
-        if len(index) != 2:
-            print("Node list must have two elements.")
-            return
-        await self.send(coord, index)
-        # for i in range(2):
-        #     await self.send(coord[i], index[i])
-
-    def run_inference(self) -> None: 
-        #Maybe pass computer vision / machine learning object in here (or in __init__)
-        #Image can also be passed here
-        #Return result of model
-        raise NotImplementedError
+        if len(messages) != len(indices):
+            raise ValueError("Length of messages and indices must be same")
+        
+        await run_parallel(*[self.send(messages[i], indices[i]) for i in indices])
 
 #Helper function to run async sequentially
 async def run_sequential(*functions: Awaitable[Any]) -> None:
@@ -176,28 +134,32 @@ async def run_parallel(*functions: Awaitable[Any]) -> None:
 
 #Test function
 async def test_connector():
-    try:  
-         
-        #Send to multiple node ID 
-        
-        cntor = Connector("opc.tcp://localhost:4840",node_id=['ns=2;i=3','ns=2;i=4'])
-        #Send to single node ID
-        #cntor = Connector(opcua_url="opc.tcp://localhost:4840",node_ids='ns=2;i=3')
-        await cntor.connect()
-        mod = Model("CC", cntor)
+    start_time = time.time()
 
+    try:  
+        nodes_1 = ['ns=2;i=2','ns=2;i=8']
+        nodes_2 = ['ns=2;i=4','ns=2;i=6']
+        cntor1 = Connector("opc.tcp://localhost:4840",node_ids=nodes_1)
+        #cntor2 = Connector("opc.tcp://localhost:4840",node_ids=['ns=2;i=6','ns=2;i=8'])
+        await cntor1.connect()
+        #await cntor2.connect()
+
+        mod1 = Model("CC", cntor1)
+        await mod1.send_multiple([100,200],[0,1])
         
-        await run_sequential(
-                # mod.send_coord((1,2),[0,1]),
-                mod.send([0,1], [0,1]),
-                run_parallel(cntor.read_value(0), cntor.read_value(1)),
-                # mod.send_coord((3,4),[0,1]), 
-                # run_parallel(cntor.read_value(0), cntor.read_value(1)),
-            )
+        #mod2 = Model("TIP", cntor2)
+        # await mod1.send(41412241421, 0),
+        # await mod2.send(142142141414,0),
+        # await mod1.send(75674, 1),
+        # await mod2.send("something",1)
         
     finally:
-        await cntor.disconnect()
-
+        await cntor1.disconnect()
+        #await cntor2.disconnect()
+    
+    end_time= time.time()
+    elapsed_time = end_time - start_time
+    print(f"Elapsed time: {elapsed_time} seconds")
 
 if __name__ == "__main__":
     try:
