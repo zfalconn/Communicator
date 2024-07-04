070eb31e1b0dccd00a81e1c36847cce1651636c0
zfalconn <linhnguyennh2210@gmail.com>
2024-07-04 09:13:51 +0200

    Test with PLC

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import util
 import model
 import asyncio
-from asyncua import Client, Node
+from asyncua import Client, Node, ua
 import json
-
+import numpy as np
 
 async def main() -> None:
     
@@ -11,29 +11,21 @@ async def main() -> None:
     data = [100,200]
     util.dump_output("CC", data, output_path) #Generate test output "2" to json file
 
-    connector = model.Connector("opc.tcp://localhost:4840",node_id=['ns=2;i=2','ns=2;i=4']) #Create Connector
-    
+    #connector = model.Connector("opc.tcp://localhost:4840",node_ids=['ns=2;i=2','ns=2;i=4']) #Create Connector
+    connector = model.Connector("opc.tcp://192.168.137.2:4840",node_ids=['ns=3;s="C-OFF_X_int"','ns=3;s="C-OFF_Y_int"'])
+
+    dv = ua.DataValue(ua.Variant(100, ua.VariantType.Double)) #convert python type to opcua type
+    dv.ServerTimestamp = None
+    dv.SourceTimestamp = None
 
     if await connector.connect(): #Check if connected to the server
         cvmodel = model.Model("CC",connector) #Attach Connector to Model
         message_to_send = util.retrieve_output(output_path) #Grab output from json file
 
-
-        print(f'Data from JSON file: {message_to_send}')
-        print(f'NodeID: {connector.node_id}\n')
-
-        print(f'Value before of {connector.node_id[0]}: {await connector.read_value(0)}') #Print output at NodeID to console
-        print(f'Value before of {connector.node_id[1]}: {await connector.read_value(1)}\n')
-
-        await cvmodel.send(message_to_send[0],0)
+        await cvmodel.send(dv,0)
        
-        print(f'Value after of {connector.node_id[0]}: {await connector.read_value(0)}') #Print output at NodeID to console
-        print(f'Value after of {connector.node_id[1]}: {await connector.read_value(1)}\n')
-        print('Some time passed...')
 
-        await cvmodel.send(message_to_send[1],1)
-        print(f'Value after of {connector.node_id[0]}: {await connector.read_value(0)}') #Print output at NodeID to console
-        print(f'Value after of {connector.node_id[1]}: {await connector.read_value(1)}\n')
+        await cvmodel.send(dv,1)
     await connector.disconnect()
 
 if __name__ == "__main__":
